d1823788859fe5fdeb3f3d853083841952069ab6
Inthrayuth Mahaphol and jeffrey hackford <jeffrey@>
2016-03-28 10:57:08 -0700

    adds looping and arrays to mad lib project

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,17 @@
 $(document).ready(function() {
   // Start Mad Lib Project
   $("#blanks form").submit(function(event) {
-    var name1Input = $("input#name1").val();
-    var name2Input = $("input#name2").val();
-    var adjectiveInput = $("input#adjective").val();
-    var nounInput = $("input#noun").val();
-    var vehicleInput = $("input#vehicle").val();
-    var verbInput = $("input#verb").val();
+    var inputs = ["name1", "name2", "adjective", "noun", "vehicle", "verb"];
 
-    $(".name1").text(name1Input);
-    $(".name2").text(name2Input);
-    $(".adjective").text(adjectiveInput);
-    $(".noun").text(nounInput);
-    $(".vehicle").text(vehicleInput);
-    $(".verb").text(verbInput);
+    inputs.forEach(function(anything) {
+      var somethingElse = $("input#" + anything).val();
+      $("." + anything).text(somethingElse);
+    });
 
     $("#story").show();
 
     event.preventDefault();
   });
   // End Mad Lib Project
-  // Start Uppercase Project
-  $("#blank-2 form").submit(function(event) {
-    var text2Input = $("textarea").val();
-    $(".text-result").text(text2Input.toUpperCase());
-
-    event.preventDefault();
-  });
   // End Uppercase Project
 });
